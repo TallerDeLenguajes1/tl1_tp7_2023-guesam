36a565cc923f3a8dcbae4b8817e7c0a6fc15a311
LucianoBonilla <lucianobonilla27@gmail.com>
2023-06-01 19:27:04 -0300

    punto 1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EspacioCalculadora;
+using System;
+string? operacion;
+double num;
 
 Calculadora cal = new Calculadora();
+
+Console.WriteLine("Ingrese la operacion que desea realizar:");
+Console.WriteLine("1. Suma");
+Console.WriteLine("2. Resta");
+Console.WriteLine("3. Multiplicacion");
+Console.WriteLine("4. Division");
+operacion = Console.ReadLine();
+Console.WriteLine("Ingrese un numero: ");
+double.TryParse(Console.ReadLine(), out num);
+cal.Suma(num);
+
+switch (operacion)
+{
+    case "1":
+    case "s":
+    case "suma":
+    Console.WriteLine("Ingrese un numero: ");
+    double.TryParse(Console.ReadLine(), out num);
+    
+
+    default:
+}
+
 cal.Suma(15);
 cal.Resta(3);
 cal.Multiplicar(3);
